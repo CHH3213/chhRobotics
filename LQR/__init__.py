21570e9906cc0d76a8dc70d4f03d2230303a3e8b
CHH3213 <1213472983@qq.com>
2022-05-06 10:15:32 +0800

    init

---
+++ LQR/__init__.py
@@ -0,0 +1,10 @@
+# --*--coding: utf-8 --*--
+"""
+Created with IntelliJ PyCharm.
+@Time: 2022/5/3 13:42
+@PROJECT_NAME: chhRobotics
+@File: __init__.py
+@Author: chh3213
+@Email:
+@Description:
+"""
